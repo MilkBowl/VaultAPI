19fe29f43da3865084b819fbe07d9c00c4e2a6eb
Nick Minkler <sleaker@gmail.com>
2014-06-23 13:20:15 -0700

    add script to publish javadocs to gh-pages branch after building new API versions on travis.

---
+++ .utility/do-publish.sh
@@ -0,0 +1,19 @@
+if [ "$TRAVIS_REPO_SLUG" != "MilkBowl/VaultAPI"] || [ "$TRAVIS_PULL_REQUEST" == "true" ] || [ "$TRAVIS_BRANCH" != "master" ]
+then
+        echo 'Travis can only publish docs for release builds.'
+        return 0
+fi
+
+# Get to the Travis build directory, configure git and clone the repo
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/MilkBowl/VaultAPI gh-pages > /dev/null
+
+# Commit and Push the Changes
+cd gh-pages
+git rm -rf *
+cp -Rf $HOME/VaultAPI/target/javadoc-latest/* ./
+git add -f .
+git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
